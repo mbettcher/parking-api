ef92dc2ce987cbda5a0cc5fefb167f9d74c98bab
mbettcher <mtonon.pmg@gmail.com>
2023-08-26 16:56:50 -0300

    Cria recurso para criar um novo usuário

--- src/main/java/br/com/mtonon/parkingapi/controller/UsuarioController.java
+++ src/main/java/br/com/mtonon/parkingapi/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package br.com.mtonon.parkingapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mtonon.parkingapi.entity.Usuario;
 import br.com.mtonon.parkingapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioController {
 	
 	private final UsuarioService usuarioService;
+	
+	@PostMapping
+	public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+		Usuario user = this.usuarioService.save(usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	}
 
 }
--- src/main/java/br/com/mtonon/parkingapi/service/UsuarioService.java
+++ src/main/java/br/com/mtonon/parkingapi/service/UsuarioService.java
@@ -1,7 +1,9 @@
 package br.com.mtonon.parkingapi.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.mtonon.parkingapi.entity.Usuario;
 import br.com.mtonon.parkingapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +12,10 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioService {
 	
 	private final UsuarioRepository usuarioRepository;
+	
+	@Transactional
+	public Usuario save(Usuario usuario) {
+		return this.usuarioRepository.save(usuario);
+	}
 
 }
