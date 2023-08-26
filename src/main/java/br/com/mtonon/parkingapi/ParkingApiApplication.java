c57631bbd7976c2410855bef90ccd0733420c268
mbettcher <mtonon.pmg@gmail.com>
2023-08-26 15:57:07 -0300

    Criação do Projeto Parking-api

---
+++ src/main/java/br/com/mtonon/parkingapi/ParkingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.parkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApiApplication.class, args);
+	}
+
+}
